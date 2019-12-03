23020167186e5e56572230f75642008335099c64
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-03 01:18:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
 
 class Dog
-  def initialize(name)
-    @name = name
-  end
-  
   def name
     @name
   end
   
-  def name=(dname)
-    @name = dname
+  def name=(name)
+    @name = name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
   end
 end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(n)
+    @name = n
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(gig)
+    @job = gig
+  end
+  
+  def job
+    @job
+  end
+end
